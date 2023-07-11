f52922f1fedbbe7f6caac2680d37405de81f907a
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-07-11 15:52:52 +0430

    Database and table created

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
-    name varchar(100)
+    id SERIAL NOT NULL,
+    name VARCHAR(40),
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(10,2),
+    PRIMARY KEY (id)
 );
